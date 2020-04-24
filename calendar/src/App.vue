08a43cd503dc3ed3f6a8c7d23d7f5e190ee120dc
namlv <namlv@smartosc.com>
2020-04-24 19:36:33 +0700

    demo

---
+++ calendar/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <div id="calendar-week" class="container">
+    </div>
+    <div id="calendar-entry">
+    <!-- Markup for calendar entry -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+html, body {
+  height: 100%;
+}
+#app {
+}
+#calendar-week {
+}
+#calendar-entry {
+}
+</style>
